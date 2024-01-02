1da5528178ebdaeb6fd9bbffb02d12a65219b930
dominhhieu2906 <hieus2computer@gmail.com>
2024-01-02 19:39:08 +0700

    hieupc1

---
+++ view/fogot-pass-view.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Quên Mật Khẩu</title>
+    <link rel="stylesheet" href="../node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/reset.css">
+    <link rel="stylesheet" href="../assets/fonts/font.css">
+    <link rel="stylesheet" href="../assets/css/main.css">
+</head>
+<body>
+    <div class="fogot-pass-view">
+        <div class="fogot-pass-view-content">
+            <h1>QUÊN MẬT KHẨU</h1>
+            <div class="input-email">
+                <i class="bi bi-envelope"></i>
+                <input type="email" placeholder="Email">
+                <i class="send-code bi bi-arrow-right"></i>
+            </div>
+            <span>Mã xắc thực 6 số</span><br>
+            <input class="verification-codes" type="text" placeholder="Mã xắc thực">
+            <div class="checkbox-dksd">
+                <input id="remember-pass" type="checkbox">
+                <label for="remember-pass">Tôi đồng ý với điều khoản sử dụng</label>
+            </div>
+            <button class="btn-fogot-pass" >Xác nhận</button>
+        </div>
+    </div>
+</body>
+</html>
